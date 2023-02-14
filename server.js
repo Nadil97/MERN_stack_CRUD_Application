777c8e9eaec604aefb4ce4ce424abbef130db820
nadil sa <nadilsandaruwan97@gmail.com>
2023-02-14 16:05:18 +0530

    database Connected

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express =require('express');
+const mongoose=require('mongoose');
 
 const app =express();
 
 
-const port =8000;
 
-app.listen(port, ()=>{
-    console.log(`App is Running on ${port}`)
+
+const PORT =8000;
+const DB_URL='mongodb+srv://nadil80:1997@cluster0.stiuiem.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL).then(()=>{
+    console.log("Database Conected");
+})
+.catch((err)=>{
+    console.log("Conection failed");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App is Running on ${PORT}`);
 });
