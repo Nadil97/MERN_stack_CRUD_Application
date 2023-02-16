07bcab04c70ef929e6d99e7ec0c1141cdd346aa4
nadil sa <nadilsandaruwan97@gmail.com>
2023-02-17 01:00:29 +0530

    use cors package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser =require('body-parser');
+const cors=require('cors');
 
 const app =express();
 // import routes 
@@ -8,6 +9,7 @@ const postRoutes =require('./routes/posts');
 
 //app middleware
 app.use(bodyParser.json());
+app.use(cors());
 app.use(postRoutes );
 
 
