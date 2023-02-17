e0000b5711f1500658a17af168c3670578775209
nadil sa <nadilsandaruwan97@gmail.com>
2023-02-17 11:01:31 +0530

    connect the database react page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            posts:[]
+        };
+    }
+
+    componentDidMount(){
+        this.retrievePosts();
+    }
+
+    retrievePosts(){
+        axios.get("http://localhost:8000/post").then(res =>{
+            if(res.data.success){
+                this.setState({
+                    posts:res.data.existingPosts
+                });
+
+                console.log(this.state.posts)
+            }
+        })
+    }
   render() {
     return (
       <div>App</div>
